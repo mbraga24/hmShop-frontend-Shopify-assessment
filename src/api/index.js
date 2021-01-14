9bd47a318c3a2666b424660f09de36dcff7360bc
github username <rdg.brg@gmail.com>
2021-01-14 18:43:22 -0500

    Api calls folder

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+export const signup = data => {
+  return fetch(`http://localhost:3000/api/v1/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(r => r.json())
+}
