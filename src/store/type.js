5031102885a8da487c5c3c9a6640e7d76e95de55
github username <rdg.brg@gmail.com>
2021-01-17 00:25:32 -0500

    Add products to redux store

--- src/store/type.js
+++ src/store/type.js
@@ -3,3 +3,5 @@ export const LOGGED_IN = "LOGGED_IN";
 
 // ================= PRODUCTS =================
 export const SET_PRODUCTS = "SET_PRODUCTS"; 
+export const ADD_PRODUCT = "ADD_PRODUCT"; 
+export const REMOVE_PRODUCT = "REMOVE_PRODUCT"; 
